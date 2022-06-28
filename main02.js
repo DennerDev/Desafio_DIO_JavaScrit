bcb1ad0c0521a930389a5dfb6ff213b8ff340402
Denner Pinho <Dennerpinho@gmail.com>
2022-06-28 12:42:52 -0300

    First Commit

---
+++ main02.js
@@ -0,0 +1,11 @@
+// var a = 3.5
+// var b  = 7.5
+
+// //TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+// var media =  (a + b) / 2;
+// console.log("MEDIA = " +   media.toFixed(5));
+
+
+let line = gets(); // Retorna a próxima linha de entrada
+console.log(line); // Imprime o dado
---
+++ main03.js
@@ -0,0 +1,18 @@
+// // a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// // Abaixo segue um exemplo de código que você pode ou não utilizar
+
+// const TT = 3.14159;
+// var raio = parseFloat(gets());
+
+// //TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+// var area =             *  Math.pow(       ,2);
+// print("A=" +                 .toFixed(4));
+
+
+const TT = 3.14159;
+
+var raio = parseFloat(gets());
+var area = TT *  Math.pow( raio ,2);
+
+console.log("A=" +  area.toFixed(4));
